1ee75a11ab55ade1f00e33e591f2b8374efe60c8
Obo Agboghidi <obo@Obos-MacBook-Pro.local>
2017-05-18 18:34:04 -0700

    Thursday commit

--- database/music.sql
+++ database/music.sql
@@ -1,78 +1,70 @@
-DROP DATABASE IF EXISTS music;
-CREATE DATABASE music;
-
-CREATE TABLE album(ID INTEGER PRIMARY KEY,
-albumTitle VARCHAR,
-songCount INTEGER,
-genre VARCHAR,
-artist VARCHAR
+DROP TABLE IF EXISTS album;
+CREATE TABLE album(
+id SERIAL PRIMARY KEY,
+album_title VARCHAR(255),
+song_count INTEGER,
+genre VARCHAR(255),
+artist VARCHAR(255)
 );
 
-CREATE TABLE artist(ID INTEGER PRIMARY KEY,
-name VARCHAR,
-bandmembers VARCHAR,
-genre VARCHAR,
-Artist_Album VARCHAR
+DROP TABLE IF EXISTS artist;
+CREATE TABLE artist(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+band_members VARCHAR(255),
+genre VARCHAR(255),
+artist_album VARCHAR(255)
 );
 
-CREATE TABLE songs(ID INTEGER PRIMARY KEY,
-song_Title VARCHAR,
+DROP TABLE IF EXISTS songs;
+CREATE TABLE songs(
+id SERIAL PRIMARY KEY,
+song_title VARCHAR(255),
 length INTEGER,
-song_Count INTEGER
+song_count INTEGER,
+artist VARCHAR(255)
 );
 
-INSERT INTO album VALUES(1, 'The Search for Everything', 12, 'Rock and Roll/Blues', 'John Mayer' );
-INSERT INTO album VALUES(2, 'Where does this door go' , 12,'Neo-Soul', 'Mayer hawthorne' );
-INSERT INTO album VALUES(3, 'Good Kid/Mad City', 16, 'Rap/HipHop', 'Kendrick Lamarr' );
-INSERT INTO album VALUES(4, 'Radius' , 13,'Neo-Soul', 'Allen Stone' );
-INSERT INTO album VALUES(5, 'Black Holes and Revelations' , 12,'Rock and Roll', 'Muse' );
-INSERT INTO album VALUES(6, 'Cool Uncle' , 13,'Neo-Soul', 'Bobby Caldwell & Jack Splash' );
-INSERT INTO album VALUES(7, 'The College Dropout' , 21,'Rap/HipHop', 'Kanye West');
-INSERT INTO album VALUES(8, 'Girl' , 12,'Pop/R&B', 'Pharrell' );
-INSERT INTO album VALUES(9, 'Lionese/Hidden treasures' , 13,'Pop/Neo-Soul', 'Amy Winehouse' );
-INSERT INTO album VALUES(10, 'The 20/20 Experience' , 13,'Pop/R&B', 'Justin Timberlake' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('The Search for Everything', 12, 'Rock and Roll/Blues', 'John Mayer' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('Where does this door go', 12, 'Neo-Soul', 'Mayer hawthorne' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('Good Kid/Mad City', 16, 'Rap/HipHop', 'Kendrick Lamarr' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('Radius', 13, 'Neo-Soul', 'Allen Stone' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('Black Holes and Revelations', 12, 'Rock and Roll', 'Muse' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('Cool Uncle', 13, 'Neo-Soul', 'Bobby Caldwell & Jack Splash' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('The College Dropout', 21, 'Rap/HipHop', 'Kanye West');
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('Girl', 12, 'Pop/R&B', 'Pharrell' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('Lionese/Hidden treasures' , 13, 'Pop/Neo-Soul', 'Amy Winehouse' );
+INSERT INTO album( album_title, song_count, genre, artist ) VALUES('The 20/20 Experience', 13, 'Pop/R&B', 'Justin Timberlake' );
+
+INSERT INTO artist( name, band_members, genre, artist_album ) VALUES('John Mayer', 'Dead and Company', 'Rock and Roll/Blues', 'The Search for Everything');
+INSERT INTO artist( name, band_members, genre, artist_album ) VALUES('Mayer Hawthorne', 'The County', 'Neo-Soul', 'Where does this door go' );
+INSERT INTO artist( name, band_members, genre, artist_album ) VALUES('Kendrick Lamarr', 'Black Hippy', 'Rap/HipHop', 'The Search for Everything' );
+
+INSERT INTO songs( song_title, length, song_count, artist ) VALUES('Rosie', 4, 12, 'John Mayer' );
+INSERT INTO songs( song_title, length, song_count, artist ) VALUES('Backseat Lover', 3, 12, 'Mayer Hawthorne' );
+INSERT INTO songs( song_title, length, song_count, artist ) VALUES('Drink', 6, 16, 'Kendrick Lamarr' );
+INSERT INTO songs( song_title, length, song_count, artist ) VALUES('Viva La Gadda', 17, 2, 'Mars Volta' );
+INSERT INTO songs( song_title, length, song_count, artist ) VALUES('Helpless', 3, 12, 'John Mayer' );
+INSERT INTO songs( song_title, length, song_count, artist ) VALUES('Take a Bow', 4, 3, 'Muse' );
+INSERT INTO songs( song_title, length, song_count, artist ) VALUES('Supermassice Black Hole', 4, 6, 'Muse' );
 
 
-
-
-
-
-INSERT INTO artist VALUES(1, 'John Mayer', 'Dead and Company', 'Rock and Roll/Blues', 'The Search for Everything');
-INSERT INTO artist VALUES(2, 'Mayer Hawthorne', 'The County', 'Neo-Soul','Where does this door go' );
-INSERT INTO artist VALUES(3, 'Kendrick Lamarr', NULL, 'Rap/HipHop','The Search for Everything' );
-
-INSERT INTO songs VALUES(1, 'Rosie', 4, 12 );
-INSERT INTO songs VALUES(2, 'Backseat Lover', 3, 12 );
-INSERT INTO songs VALUES(3, 'Drink', 6, 16 );
-INSERT INTO songs VALUES(4, 'Viva La Gadda', 17, 2 );
-INSERT INTO songs VALUES(5, 'Helpless', 3, 12 );
-INSERT INTO songs VALUES(6, 'Take a Bow', 4, 3 );
-INSERT INTO songs VALUES(7, 'Supermassice Black Hole', 4, 6 );
-
-
-
-INSERT INTO playlists VALUES(1, 'Blues',36 );
-INSERT INTO playlists VALUES(2, 'Soul/R&B',76 );
-INSERT INTO playlists VALUES(3, 'Rap',22 );
-
-
-
-SELECT albumTitle FROM album;
-SELECT * FROM playlist;
-SELECT * FROM songs;
-SELECT * FROM artist;
-
-SELECT song_title FROM songs WHERE length < 4 AND songCount > 10;
-SELECT songs FROM playlist ORDER BY songs;
-SELECT song_Title FROM songs ;
-
-SELECT artist FROM album WHERE genre IN ('Neo-Soul');
-
-
-SELECT albumTitle, songCount FROM album
-ORDER BY songCount DESC
-LIMIT 3;
-
-SELECT songs, SUM(songs) AS total_songs FROM playlists
-        GROUP BY Playlist_name
-        HAVING total_num_songs > 15;
+-- SELECT albumTitle FROM album;
+-- SELECT * FROM playlist;
+-- SELECT * FROM songs;
+-- SELECT * FROM artist;
+--
+-- SELECT song_title FROM songs WHERE length < 4 AND songCount > 10;
+-- SELECT songs FROM playlist ORDER BY songs;
+-- SELECT song_Title FROM songs ;
+--
+-- SELECT artist FROM album WHERE genre IN ('Neo-Soul');
+--
+--
+-- SELECT albumTitle, songCount FROM album
+-- ORDER BY songCount DESC
+-- LIMIT 3;
+--
+-- SELECT songs, SUM(songs) AS total_songs FROM playlists
+--         GROUP BY Playlist_name
+--         HAVING total_num_songs > 15;
--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,38 @@ var db = require('../database/queries')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.send("Music_Player beta v1.0.0")
+  res.render('index.pug', { title: 'Music Player' });
+  //res.send("Music_Player beta v1.0.0")
 });
 
 router.get('/api/music', db.getAllMusic);
 router.get('/api/music/songs/:id', db.getSingleSong);
+
+
+
 router.get('/api/music/album/:id', db.getSingleAlbum);
-router.post('/api/music', db.createSong);
+router.post('/api/music/add/', function( req, res ) {
+  const { song_title, artist } = req.body
+  console.log(req.body);
+  //const song_title = req.body.song_title
+  //const length = req.body.length
+  //const song_count = req.body.song_count
+  console.log( 'TITLE', song_title )
+  //console.log( 'LENGTH', length )
+  //console.log( 'COUNT', song_count )
+  console.log( 'BODY ', req.body)
+
+  db.createSong( song_title, artist )
+    .then( function() { res.status(201).json({
+          status: 'success',
+          message: 'Inserted one song'
+        })
+      })
+    .catch( function( error ) {
+      res.send( error )
+    })
+})
+
 router.put('/api/music', db.updateSong);
 router.delete('/api/music', db.removeSong);
 
